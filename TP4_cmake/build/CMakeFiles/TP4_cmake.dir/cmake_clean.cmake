ac6cffe253f1cb0e1c9c4281718f5f98642161b2
/fcg <stasyanrus610@gmail.com>
2023-05-26 20:06:12 +0300

    cmake

---
+++ TP4_cmake/build/CMakeFiles/TP4_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP4_cmake.dir/main.cpp.o"
+  "CMakeFiles/TP4_cmake.dir/main.cpp.o.d"
+  "TP4_cmake"
+  "TP4_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP4_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TP4_cmake/plenty.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+
+
+template <typename T>
+class plenty {
+private:
+    int size;
+    T* arr;
+public:
+    plenty(int _size, int choose);
+    void print();
+
+    //!= проверка на неравенство множеств
+    bool operator!=(const plenty& L) const;
+
+    //!= проверка на равенство множеств
+    bool operator==(const plenty& L) const;
+
+    T& operator [] (int index);
+
+};
